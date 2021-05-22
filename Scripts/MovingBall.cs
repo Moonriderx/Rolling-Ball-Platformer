d581e4723d1a84243fd8be47f5308489677fd5d3
Moonriderx <luben1993@abv.bg>
2021-05-23 00:31:18 +0300

    Added Rigidbody support and simple jump

--- Scripts/MovingBall.cs
+++ Scripts/MovingBall.cs
@@ -6,26 +6,27 @@ namespace Moonrider
     public class MovingBall : MonoBehaviour
     {
 
+        Rigidbody body;
+
         [SerializeField, Range(0f, 100f)]
         float maxSpeed = 10f;
 
         [SerializeField, Range(0f, 100f)]
         float maxAcceleration = 10f;
 
-        [SerializeField]
-        Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
-
-        [SerializeField, Range(0f, 1f)]
-        float bounciness = 0.5f;
-
+        [SerializeField, Range(0f, 10f)]
+        float jumpHeight = 2f;
 
         Vector3 velocity;
+        Vector3 desiredVelocity;
+        bool desiredJump;
+        public bool onGround;
 
 
         // Start is called before the first frame update    
         void Start()
         {
-
+            body = GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
@@ -36,36 +37,50 @@ namespace Moonrider
             playerInput.y = Input.GetAxis("Vertical");
             playerInput = Vector2.ClampMagnitude(playerInput, 1f); // made the position inside the "circle" valid too.
 
-            Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-            float maxSpeedChange = maxAcceleration * Time.deltaTime;
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            desiredJump |= Input.GetButtonDown("Jump");
+        }
 
-            Vector3 displacement = velocity * Time.deltaTime;
-            Vector3 newPosition = transform.localPosition + displacement;
-            if (newPosition.x < allowedArea.xMin)
-            {
-                newPosition.x = allowedArea.xMin;
-                velocity.x = -velocity.x * bounciness;
-            }
-            else if (newPosition.x > allowedArea.xMax)
-            {
-                newPosition.x = allowedArea.xMax;
-                velocity.x = -velocity.x * bounciness;
-            }
-            if(newPosition.z < allowedArea.yMin)
-            {
-                newPosition.z = allowedArea.yMin;
-                velocity.z = -velocity.z * bounciness;
-            }
-            else if (newPosition.z > allowedArea.yMax)
-            {
-                newPosition.z = allowedArea.yMax;
-                velocity.z = -velocity.z * bounciness;
-            }
-            transform.localPosition = newPosition;
+         
+
+        private void FixedUpdate()
+        {
+
+            velocity = body.velocity;
+            float maxSpeedChange = maxAcceleration * Time.deltaTime;
             velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
             velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+            body.velocity = velocity;
+
+            if (desiredJump)
+            {
+                desiredJump = false;
+                Jump();
+            }
+            body.velocity = velocity;
+            onGround = false;
 
         }
+
+        void Jump()
+        {
+            if (onGround)
+            {
+                velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            onGround = true;
+        }
+
+        private void OnCollisionStay(Collision collision)
+        {
+            onGround = true;   
+        }
     }
-}
+
+    }
+
 
